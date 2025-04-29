c9f20f3e3c4024e683dc3f8c63112d6514eda642
ginogen <ginogentileg@live.com>
2025-04-29 16:37:00 -0300

    netlify

--- src/components/modals/OrganizationModal.tsx
+++ src/components/modals/OrganizationModal.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
-import { Button } from '../../components/ui/button';
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../../components/ui/select';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 type Organization = {
   id: string;
